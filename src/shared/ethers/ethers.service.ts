d0c566ad722673c9fa9e05d924805d16a7350c5b
xiith <xiith@whour.co>
2024-09-11 15:25:52 +0700

    mod

--- src/shared/ethers/ethers.service.ts
+++ src/shared/ethers/ethers.service.ts
@@ -3,19 +3,6 @@ import { ethers } from 'ethers';
 
 @Injectable()
 export class EthersService implements OnModuleInit {
-  // private async provider(): Promise<ethers.JsonRpcProvider> {
-  //   const rpcURL = process.env.SEPOLIA_ALCHEMY;
-  //   const provider = new ethers.JsonRpcProvider(rpcURL);
-  //   console.log('provider created');
-  //   return provider;
-  // }
-
-  // private async signer(provider: ethers.JsonRpcProvider): Promise<ethers.Wallet> {
-  //   const privateKey = process.env.SINGER_KEY;
-  //   const signer = new ethers.Wallet(privateKey, provider);
-  //   console.log('signer created');
-  //   return signer}
-
   public provider: ethers.JsonRpcProvider;
   public signer: ethers.Wallet;
 
