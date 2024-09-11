be3c9bf92b689af2888e1379409e6b005f9f7643
xiivth <158536134+xiivth@users.noreply.github.com>
2024-09-11 15:30:14 +0700

    app

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -4,13 +4,7 @@ import { EthersService } from './shared/ethers/ethers.service';
 
 @Controller()
 export class AppController {
-  constructor(
-    private readonly appService: AppService,
-    private readonly ethersService: EthersService,
-  ) {
-    this.ethersService.connectEthereum();
-  }
-
+  
   @Get()
   getHello(): string {
     return this.appService.getHello();
